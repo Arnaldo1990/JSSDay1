8cf5accfc371fdb9597ca99ef61cf4cf0541e52a
Donatella Tesei <donatella.tesei@gmail.com>
2023-02-01 11:56:24 +0100

    first push

---
+++ Donatella/index.js
@@ -0,0 +1,5 @@
+let age = "25";
+console.log(`Hi my name is Martin and I am ${age} years old`);
+
+let players = ['Martin', 'Thomas', 'Peter', 'Mathias', 'Niki'];
+console.log(`The most valuable player of the match is ${players[2]}`);
