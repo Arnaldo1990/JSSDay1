8a2d39a107b1a8e5aa544e104da06c38d36a65e2
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-02-01 14:48:23 +0100

    changes

---
+++ Arnaldo/advanced.js
@@ -0,0 +1,17 @@
+let a = 7;
+
+let b = 14;
+
+let c = "21";
+
+let d = '36';
+
+let e = 42;
+
+let newC = Number(c);
+console.log(typeof newC);
+let newD = Number(d);
+console.log(typeof newD);
+console.log(a + b + newC + newD + e);
+let sum = (a + b + newC + newD + e);
+console.log(sum);
